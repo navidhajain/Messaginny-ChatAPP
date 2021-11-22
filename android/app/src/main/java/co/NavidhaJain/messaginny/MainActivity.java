1c75fee53f91aac5286078ac63ba2e2774e1bbaf
navidhajain <88448330+navidhajain@users.noreply.github.com>
2021-11-22 17:14:05 +0530

    init repo

---
+++ android/app/src/main/java/co/NavidhaJain/messaginny/MainActivity.java
@@ -0,0 +1,11 @@
+package co.NavidhaJain.messaginny;
+
+import android.os.Bundle;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
+
+public class MainActivity extends FlutterActivity {
+}
